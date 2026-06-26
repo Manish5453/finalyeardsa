bd64e9162c8c1ad6d32cdd1e4128291fd4c9ac63
Manish Jha <132031234+Manish5453@users.noreply.github.com>
2026-06-26 19:38:27 +0530

    Create Q11.cpp

---
+++ Q11.cpp
@@ -0,0 +1,25 @@
+//The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+//such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+//F (0) = 0, F(1) = 1
+//F(n) = F(n - 1) + F(n - 2), for n > 1.
+//Given n, calculate F(n)
+#include <iostream>
+using namespace std;
+int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        int p1=0, p2=1;
+
+        for(int i =2;i<=n;i++){
+            int curr = p1+p2;
+            p1=p2;
+            p2=curr;
+        }
+        cout<<p2;
+        return p2;
+}
+int main() {
+    fib(6);
+    return 0;
+}
